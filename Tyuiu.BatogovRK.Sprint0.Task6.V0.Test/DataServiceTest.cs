5af1be3d6da9d4bb00acee5135937e64332d8e15
Tyuiu.BatogovRK@outlook.com <romanbatogov@gmail.com>
2025-09-15 16:05:42 +0500

    Task6

---
+++ Tyuiu.BatogovRK.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.BatogovRK.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.BatogovRK.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionArrayValid()
+        {
+            var numbers= new int[] {1,2,3,4,5};
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+        [TestMethod]
+        public void CheckeSubstractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubstractionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+        [TestMethod]
+        public void CheckedMultArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+    }
+}
---
+++ Tyuiu.BatogovRK.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BatogovRK.Sprint0.Task6.V0.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numsArray = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Сумма элементова массива = " +DataService.AdditionArray(numsArray));
+        Console.WriteLine("Разность элементов массива = " +DataService.SubstractionArray(numsArray));
+        Console.WriteLine("Произведение элементов массива = " +DataService.MultArray(numsArray));
+        Console.ReadKey();
+
+    }
+}
